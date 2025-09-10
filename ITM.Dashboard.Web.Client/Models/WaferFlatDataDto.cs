1d07d277f6c348a6af34219da82eb19824b38285
greatit-lab <gil2zum@gmail.com>
2025-09-11 08:30:43 +0900

    Update WaferFlatDataDto.cs -- 0911

--- ITM.Dashboard.Web.Client/Models/WaferFlatDataDto.cs
+++ ITM.Dashboard.Web.Client/Models/WaferFlatDataDto.cs
@@ -6,10 +6,6 @@ namespace ITM.Dashboard.Web.Client.Models
         public string? LotId { get; set; }
         public int? WaferId { get; set; }
         public DateTime? DateTime { get; set; }
-        public int Point { get; set; }
-        public double X { get; set; }
-        public double Y { get; set; }
-
         public string? CassetteRcp { get; set; }
         public string? StageRcp { get; set; }
         public string? StageGroup { get; set; }
