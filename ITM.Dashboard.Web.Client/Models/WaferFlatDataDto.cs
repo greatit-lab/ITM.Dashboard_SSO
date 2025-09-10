089929b0c51b23daa15d6f330eff109ea0a72254
greatit-lab <gil2zum@gmail.com>
2025-09-10 18:58:26 +0900

    Create WaferFlatDataDto.cs

---
+++ ITM.Dashboard.Web.Client/Models/WaferFlatDataDto.cs
@@ -0,0 +1,18 @@
+// ITM.Dashboard.Web.Client/Models/WaferFlatDataDto.cs
+namespace ITM.Dashboard.Web.Client.Models
+{
+    public class WaferFlatDataDto
+    {
+        public string? LotId { get; set; }
+        public int? WaferId { get; set; }
+        public DateTime? DateTime { get; set; }
+        public int Point { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public string? CassetteRcp { get; set; }
+        public string? StageRcp { get; set; }
+        public string? StageGroup { get; set; }
+        public string? Film { get; set; }
+    }
+}
