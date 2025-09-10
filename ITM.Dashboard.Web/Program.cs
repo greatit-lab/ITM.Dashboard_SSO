89f06907050c40ba1a332b4b5b53335970da3eed
greatit-lab <gil2zum@gmail.com>
2025-09-10 18:43:11 +0900

    Update Program.cs -- 0910

--- ITM.Dashboard.Web/Program.cs
+++ ITM.Dashboard.Web/Program.cs
@@ -1,30 +1,24 @@
-// 파일 경로: ITM.Dashboard.Web/Program.cs
+// ITM.Dashboard.Web/Program.cs
 
 using ITM.Dashboard.Web.Client.Pages;
 using ITM.Dashboard.Web.Components;
 using MudBlazor.Services;
-using System;
-using System.Net.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
 
 builder.Services.AddMudServices();
 
-// 서버 측 렌더링을 위해 HttpClient를 등록합니다.
+// ▼▼▼ "ITM.Dashboard.Api" 라는 이름으로 HttpClient를 등록합니다 ▼▼▼
 builder.Services.AddHttpClient("ITM.Dashboard.Api", client =>
 {
-    // API 서버의 주소를 입력합니다.
-    client.BaseAddress = new Uri("https://localhost:7278"); 
+    client.BaseAddress = new Uri("https://localhost:7278"); // API 서버 주소
 });
-
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
@@ -32,12 +26,10 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
