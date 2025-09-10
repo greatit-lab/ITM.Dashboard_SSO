6a4cb8d7342510919c9536253ed0a3bed871f97c
greatit-lab <gil2zum@gmail.com>
2025-09-10 18:56:29 +0900

    Create DateRangeDto.cs

---
+++ ITM.Dashboard.Web.Client/Models/DateRangeDto.cs
@@ -0,0 +1,11 @@
+// ITM.Dashboard.Web.Client/Models/DateRangeDto.cs
+using System;
+
+namespace ITM.Dashboard.Web.Client.Models
+{
+    public class DateRangeDto
+    {
+        public DateTime? MinDate { get; set; }
+        public DateTime? MaxDate { get; set; }
+    }
+}
