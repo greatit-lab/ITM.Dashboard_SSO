8bf9bbbd1d0fdcae55ac2b09602fc1d5364ac0ad
greatit-lab <gil2zum@gmail.com>
2025-09-10 18:13:53 +0900

    Create DateRangeDto.cs

---
+++ ITM.Dashboard.Api/Models/DateRangeDto.cs
@@ -0,0 +1,11 @@
+// 파일 경로: ITM.Dashboard.Api/Models/DateRangeDto.cs
+using System;
+
+namespace ITM.Dashboard.Api.Models
+{
+    public class DateRangeDto
+    {
+        public DateTime? MinDate { get; set; }
+        public DateTime? MaxDate { get; set; }
+    }
+}
