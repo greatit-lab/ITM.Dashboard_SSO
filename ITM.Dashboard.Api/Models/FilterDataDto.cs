1a49e49407d53c82bebc883e2c61ee76b1081a59
greatit-lab <gil2zum@gmail.com>
2025-09-10 18:14:36 +0900

    Create FilterDataDto.cs

---
+++ ITM.Dashboard.Api/Models/FilterDataDto.cs
@@ -0,0 +1,11 @@
+// 파일 경로: ITM.Dashboard.Api/Models/FilterDataDto.cs
+namespace ITM.Dashboard.Api.Models
+{
+    public class FilterDataDto
+    {
+        // string 뒤에 '?'를 붙여 Null을 허용하도록 설정합니다.
+        public string? Site { get; set; }
+        public string? Sdwt { get; set; }
+        public string? EqpId { get; set; }
+    }
+}
