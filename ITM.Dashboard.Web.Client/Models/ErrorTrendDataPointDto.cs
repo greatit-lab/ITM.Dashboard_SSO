c843a77c1a9045d7531b0c9d5c8b8ebfa5c9ea0d
greatit-lab <gil2zum@gmail.com>
2025-09-15 16:35:33 +0900

    Create ErrorTrendDataPointDto.cs

---
+++ ITM.Dashboard.Web.Client/Models/ErrorTrendDataPointDto.cs
@@ -0,0 +1,11 @@
+// ITM.Dashboard.Web.Client/Models/ErrorTrendDataPointDto.cs
+using System;
+
+namespace ITM.Dashboard.Web.Client.Models
+{
+    public class ErrorTrendDataPointDto
+    {
+        public DateTime Date { get; set; }
+        public long Count { get; set; }
+    }
+}
