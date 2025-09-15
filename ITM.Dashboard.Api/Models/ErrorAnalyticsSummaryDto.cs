05c59201202322df70134237d7c44bf0f45d1d68
greatit-lab <gil2zum@gmail.com>
2025-09-15 16:22:39 +0900

    Create ErrorAnalyticsSummaryDto.cs

---
+++ ITM.Dashboard.Api/Models/ErrorAnalyticsSummaryDto.cs
@@ -0,0 +1,20 @@
+// ITM.Dashboard.Api/Models/ErrorAnalyticsSummaryDto.cs
+using System.Collections.Generic;
+
+namespace ITM.Dashboard.Api.Models
+{
+    public class ErrorAnalyticsSummaryDto
+    {
+        public long TotalErrorCount { get; set; }
+        public int ErrorEqpCount { get; set; }
+        public string TopErrorId { get; set; }
+        public int TopErrorCount { get; set; }
+        public List<ChartDataItem> ErrorCountByEqp { get; set; } = new();
+    }
+
+    public class ChartDataItem
+    {
+        public string Label { get; set; }
+        public double Value { get; set; }
+    }
+}
